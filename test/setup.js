5ad4268b52522222c0960f2433258f5d16649247
MinJieLiu <liumingyi_1@163.com>
2017-03-06 22:55:20 +0800

    add test .

--- test/setup.js
+++ test/setup.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, render, mount } from 'enzyme';
 import {
   TextApp1,
   TextApp2,
@@ -14,9 +14,20 @@ import {
 describe('Test to create a basic form', () => {
 
   it('The form is rendered correctly', () => {
-    const item = shallow(<TextApp1 />);
-    expect(item.exists()).to.equal(true);
-    expect(item.node.props.formControl).to.be.an.instanceOf(Object);
+    const app = shallow(
+      <TextApp1 values={{ email: '' }} />,
+    );
+    expect(app.exists()).to.equal(true);
+    expect(app.node.props.formControl).to.be.an.instanceOf(Object);
+  });
+
+  it('The form attribute is correct', () => {
+    const app = render(
+      <TextApp1 values={{ email: '' }} />,
+    );
+    const item = app.find('#email');
+    expect(item.attr('name')).to.equal('email');
+    expect(item.attr('id')).to.equal('email');
   });
 
 });
